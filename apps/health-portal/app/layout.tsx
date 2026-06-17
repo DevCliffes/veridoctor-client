c9f2c9c91cd69754513bf48b18e03313971de99c
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-17 12:12:56 +0300

    Add Toaster component to RootLayout

--- apps/health-portal/app/layout.tsx
+++ apps/health-portal/app/layout.tsx
@@ -1,8 +1,7 @@
 import "./globals.css";
 import "@veridoctor/design/styles/globals.css";
-
 import StoreProvider from "./StoreProvider";
-
+import { Toaster } from "sonner";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -13,6 +12,7 @@ export default function RootLayout({
       <StoreProvider>
         <body className="antialiased font-sans">
           <div>{children}</div>
+          <Toaster richColors position="top-right" />
         </body>
       </StoreProvider>
     </html>

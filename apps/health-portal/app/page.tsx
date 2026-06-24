9fe074e783349c7131647eb91cdf477ee892057c
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-24 10:49:07 +0300

    Add redirect functionality after authentication
    Redirect users to a specified page if a redirect parameter is present in the URL.

--- apps/health-portal/app/page.tsx
+++ apps/health-portal/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useRouter, useSearchParams } from "next/navigation";
 import { useAppDispatch } from "./hooks";
 import { useEffect, Suspense } from "react";
@@ -14,7 +13,13 @@ function HomeContent() {
   useEffect(() => {
     dispatch(setAuthCode(searchParams.get("auth_tkn")));
     dispatch(setUserId(searchParams.get("identity")));
-    router.push("/dashboard");
+
+    // If the user was sent here from the public homepage "View profile"
+    // button, a `redirect` param will be present, e.g:
+    //   ?auth_tkn=...&identity=...&redirect=/book/provider/abc-123
+    // Send them there instead of the default dashboard.
+    const redirect = searchParams.get("redirect");
+    router.push(redirect ?? "/dashboard");
   }, []);
 
   return <PageLoader />;

db356646954e910b7d4363fb722fa7d2f8e322c5
DevCliffes <wycliffelichoti94@gmail.com>
2026-07-01 02:43:44 +0300

    Remove metadata from layout.tsx
    Removed metadata export for the layout component.

--- apps/web/app/(web)/layout.tsx
+++ apps/web/app/(web)/layout.tsx
@@ -1,12 +1,6 @@
-import type { Metadata } from "next";
 import "@veridoctor/design/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
-export const metadata: Metadata = {
-  title: "VeriDoctor",
-  description: "Revolutionizing healthcare one click at a time",
-};
 
 export default function RootLayout({
   children,

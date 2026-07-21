3d95118522dd33c8521e2860fee61e0f7126f3f4
DevCliffes <wycliffelichoti94@gmail.com>
2026-07-22 02:25:46 +0300

    Clean up StoreProvider by removing unused code
    Removed unused useEffect hook and hydrateAuth dispatch.

--- apps/telehealth/app/components/StoreProvider.tsx
+++ apps/telehealth/app/components/StoreProvider.tsx
@@ -1,17 +1,11 @@
 "use client";
-import { useEffect } from "react";
 import { Provider } from "react-redux";
 import { store } from "@/states/store/store";
-import { hydrateAuth } from "@veridoctor/store";
 
 export default function StoreProvider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  useEffect(() => {
-    store.dispatch(hydrateAuth());
-  }, []);
-
   return <Provider store={store}>{children}</Provider>;
 }
